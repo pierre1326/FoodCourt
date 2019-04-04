4b00d4b7059461de73af453f3fb1807eda6a6255
pierre1326 <pierre1326@hotmail.com>
2019-04-03 20:45:37 -0600

    Puerto
    Se soluciona el puerto utilizado por heroku para iniciar

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoURI : "mongodb://pierre1326:profesoR1@ds149875.mlab.com:49875/tec",
-  port : 5000 | process.env.PORT,
+  port : process.env.PORT || 5000,
   user : "foodcourtec@gmail.com",
   pass : "profesoR1",
   service :"gmail",
